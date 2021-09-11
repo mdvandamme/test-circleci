3561ed3f98a5aa7036f6b982113635c041fd6621
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-09-11 11:32:51 +0200

    test

--- src/math.py
+++ src/math.py
@@ -14,3 +14,12 @@ class Math():
         
         else:
             return value1 + value2
+        
+    
+    def soustraction (value1, value2):
+        
+        if not isinstance(value1, int) and not isinstance(value2, int):
+            return 'Invalid input'
+        
+        else:
+            return value1 - value2
