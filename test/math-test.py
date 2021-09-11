3c9f9ba4fd8ff5aefcd421fe5fe6e1fd2528d81c
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-09-11 11:40:56 +0200

    test

--- test/math-test.py
+++ test/math-test.py
@@ -8,7 +8,19 @@ from src.math import Math
 
 class MathTest(unittest.TestCase):
     
-    def test_addition(self):
+    def test_addition_1(self):
         # Make test fail
         self.assertEqual(Math.addition(3, 4), 8)
         
+    def test_addition_2(self):
+        # Make test fail
+        self.assertEqual(Math.addition(3, 4), 7)
+        
+    def test_soustraction_1(self):
+        # Make test fail
+        self.assertEqual(Math.soustraction(3, 4), -1)
+        
+    def test_soustraction_2(self):
+        # Make test fail
+        self.assertEqual(Math.soustraction(3, -4), 7)
+    
